927763e1f1a7a6ccc07438eb07906996935f1696
Adam Williams <lol768@lol768.com>
2019-01-20 15:58:14 +0000

    Support more versions of the Player JS

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ function listener(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   let decoder = new TextDecoder("utf-8");
   let encoder = new TextEncoder();
-  console.log("replacing J(a)...");
 
   let str = "";
   filter.ondata = event => {
@@ -11,8 +10,8 @@ function listener(details) {
 
   filter.onstop = event => {
     console.log("Trying to find video/audio override code");
-    let re = /function J\(a\)\{a\.ctrlKey&&a\.altKey&&a\.shiftKey&&83\=\=a\.keyCode&&S\.BF&&v\(\);\}/;
-    str = str.replace(re, "function J(a){a.ctrlKey&&a.altKey&&a.shiftKey&&83==a.keyCode&&v();}");
+    let re = /function ([A-Z])\(a\)\{a\.ctrlKey&&a\.altKey&&a\.shiftKey&&83\=\=a\.keyCode&&[A-Z]\.[A-Za-z$]+?&&([a-z])\(\);\}/;
+    str = str.replace(re, "function $1(a){a.ctrlKey&&a.altKey&&a.shiftKey&&83==a.keyCode&&$2();}");
     filter.write(encoder.encode(str));
     filter.disconnect();
   }
@@ -21,5 +20,5 @@ function listener(details) {
 }
 chrome.webRequest.onBeforeRequest.addListener(
   listener,
-  {urls: ["https://assets.nflxext.com/en_us/ffe/player/html/cadmium-playercore-6.0012.078.051.js"]}, ["blocking"]
+  {urls: ["https://assets.nflxext.com/en_us/ffe/player/html/cadmium-playercore-6.0012*"]}, ["blocking"]
 );
